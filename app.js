42a5d42ee2fbb35374baa8d064c2cb9645685252
AlcinaW <white_kaitou@hotmail.com>
2016-02-26 00:50:05 -0500

    Binding audio to slider wip

--- app.js
+++ app.js
@@ -1,29 +1,25 @@
-var vol_input = document.getElementById('audio-vol-input').value;
+var audioClip = document.getElementById("audioClip"); 
+var audioVolume = document.getElementbyId("audioVolume");
+
+//Grabs slider value
+var vol_input = document.getElementById('audio-vol-input');
 
 console.log(vol_input);
 
+function slideVolume() {
+    var audioClip = document.getElementById("audioClip");
+    audioClip.volume = document.getElementById("audioVolume").value;
 
-var music = document.getElementById("myAudio"); 
+}
+
 
 // On input change, number can't go below a range
-//but why write in JS when you can min max in HTML?
+//but why write in JS when you can min max in HTML5?
 // function handleChange(input) {
 // 	if (input.value < 0) input.value = 0;
 //     if (input.value > 10) input.value = 10;
 // }
 
-//Make volume = to what is in input field
+//Make volume == to what is in input field
 music.volume = vol_input;
 console.log (music.volume);
-
-//function startLoop() {
-// 	music.loop = true; 
-// 	music.load();
-// }
-
-// function stopLoop() {
-// 	music.loop = false; 
-// 	music.load();
-// }
-
-// music.volume = document.getElementById('audio-vol-display').innerHTML;
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# JavaScript Music Player
+# JavaScript Music Mixer
 
 A music player that works in-browser with a set of predetermined tracks. When buttons or parts of the graphics are manipulated, the aspects of the tracks, such as volume or speed, changes as well. There will be a visualization component that will be manipulated along with the music.  
 
