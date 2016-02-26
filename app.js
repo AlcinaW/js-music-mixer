731d19e5e71203c94757cf475dea63d0ab59e905
AlcinaW <white_kaitou@hotmail.com>
2016-02-25 22:11:57 -0500

    Volume control

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+window.onload = document.getElementById('audio-vol-input').innerHTML;
+
 var music = document.getElementById("myAudio"); 
 
-function startLoop() {
-	music.loop = true; 
-	music.load();
-}
+music.volume = .5;
 
-function stopLoop() {
-	music.loop = false; 
-	music.load();
-}
+//function startLoop() {
+// 	music.loop = true; 
+// 	music.load();
+// }
+
+// function stopLoop() {
+// 	music.loop = false; 
+// 	music.load();
+// }
+
+// music.volume = document.getElementById('audio-vol-display').innerHTML;
