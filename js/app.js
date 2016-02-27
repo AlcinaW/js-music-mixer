ff99fd8167b278b8091443f806b3e29509bbb12a
AlcinaW <white_kaitou@hotmail.com>
2016-02-26 21:09:59 -0500

    Play/Pause and set slider to 1
    - play/pause with button
    - on load, audio is set to max volume (instead of just in the middle)

--- js/app.js
+++ js/app.js
@@ -1,11 +1,30 @@
 var audioClip = document.getElementById("audioClip"); 
 var audioVolume = document.getElementById("audioVolume");
+var playButton = document.getElementById("play-pause-button");
 
 // binds audio volume to slider input
 function slideVolume() {
     audioClip.volume = audioVolume.value;
 }
 
+// lets you play or pause the audio
+function playPause() {
+    if (audioClip.paused) {
+    	audioClip.play();   
+    } else {
+        audioClip.pause();
+    }
+}
+
+//button text swaps when pressed
+function swapText() {
+    if (playButton.value === "Pause") {
+    	playButton.value = "Play";
+    }
+    else {
+    	playButton.value = "Pause";
+    }
+}
 
 // On input change, number can't go below a range
 //but why write in JS when you can min max in HTML5?
