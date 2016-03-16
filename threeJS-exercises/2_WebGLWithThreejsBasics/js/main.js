c828f6fab89d5807772c2aed70ac9480854c97bc
AlcinaW <white_kaitou@hotmail.com>
2016-03-15 23:00:11 -0400

    cont exercises

---
+++ threeJS-exercises/2_WebGLWithThreejsBasics/js/main.js
@@ -0,0 +1,9 @@
+var width = window.innerWidth;
+var height = window.innerHeight;
+
+var renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(width, height);
+document.body.appendChild(renderer.domElement);
+ 
+var scene = new THREE.Scene;
+
