fe147ef390177e4fd7bdf1a2e6605cdf9a0d5795
AlcinaW <white_kaitou@hotmail.com>
2016-03-26 11:29:42 -0400

    Added buffering

--- public/js/app.js
+++ public/js/app.js
@@ -1,34 +1,77 @@
-var audioClip = document.getElementById("audioClip"); 
-var audioVolume = document.getElementById("audioVolume");
-var playButton = document.getElementById("playPauseButton");
+window.onload = init;
+var context;
+var bufferLoader;
+
+function init() {
+  context = new webkitAudioContext();
+
+  bufferLoader = new BufferLoader(
+    context,
+    [
+      '../media/The_Voyage.mp3',
+      '../media/Every_Step.mp3',
+    ],
+    finishedLoading
+    );
+
+  bufferLoader.load();
+}
+
+function finishedLoading(bufferList) {
+  // Create two sources and play them both together.
+  var source1 = context.createBufferSource();
+  var source2 = context.createBufferSource();
+  source1.buffer = bufferList[0];
+  source2.buffer = bufferList[1];
+
+  source1.connect(context.destination);
+  source2.connect(context.destination);
+  source1.start(0);
+  source2.start(0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// var audioClip = document.getElementById("audioClip"); 
+// var audioVolume = document.getElementById("audioVolume");
+// var playButton = document.getElementById("playPauseButton");
 
 //volume test
-function outputUpdate(vol) {
-    document.querySelector('#audioVolume').value = vol;
-}
+// function outputUpdate(vol) {
+//     document.querySelector('#audioVolume').value = vol;
+// }
 
 // binds audio volume to slider input
-function slideVolume() {
-    audioClip.volume = audioVolume.value;
-}
+// function slideVolume() {
+//     audioClip.volume = audioVolume.value;
+// }
 
 // button that lets you play or pause the audio
-function playPause() {
-    if (audioClip.paused) {
-    	audioClip.play();   
-    } else {
-        audioClip.pause();
-    }
-}
+// function playPause() {
+//     if (audioClip.paused) {
+//     	audioClip.play();   
+//     } else {
+//         audioClip.pause();
+//     }
+// }
 
 //button text swaps when pressed
-function swapText() {
-    if (playButton.value === "Pause") {
-    	playButton.value = "Play";
-    }
-    else {
-    	playButton.value = "Pause";
-    }
-}
+// function swapText() {
+//     if (playButton.value === "Pause") {
+//     	playButton.value = "Play";
+//     }
+//     else {
+//     	playButton.value = "Pause";
+//     }
+// }
 
 
