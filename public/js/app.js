e32285d9e39f748524a3c41820a482636ca0e3c3
AlcinaW <white_kaitou@hotmail.com>
2016-04-02 17:04:29 -0400

    test and add css

--- public/js/app.js
+++ public/js/app.js
@@ -4,26 +4,52 @@
 
 //scene, then camera, then renderer
 
-//declare scene variable 
-var scene;
+ //Declare three.js variables
+var camera, scene, renderer, cube;
  
-//create scene
-scene = new THREE.Scene();
+//assign three.js objects to each variable
+function init(){
+    //camera
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+     
+    //scene
+    scene = new THREE.Scene();
+     
+    //renderer
+    renderer = new THREE.WebGLRenderer();
+    //set the size of the renderer
+    renderer.setSize( window.innerWidth, window.innerHeight );
+     
+    //add the renderer to the html document body
+    document.body.appendChild( renderer.domElement );
+}
 
-//declare camera variable 
-var camera;
- 
-//assign three.js perspective camera to variable
-camera = new THREE.PerspectiveCamera(75, window.innerWidth/ window.innerHeight, 0.1, 1000);
+function addCube(){
+    //create box geometry object
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    //create material with colour
+    var material = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
+    //combine geometry with material to create the cube
+    cube = new THREE.Mesh( geometry, material );
+    //add the cube to the scene
+    scene.add( cube );
 
-//declare renderer variable 
-var renderer;
- 
-//assign WebGL renderer object
-renderer = new THREE.WebGLRenderer();
- 
-//set the size of the renderer
-renderer.setSize( window.innerWidth, window.innerHeight );
- 
-//add the renderer to the html document body element
-document.body.appendChild( renderer.domElement );
+    //set the camera position
+    camera.position.z = 5;
+}
+
+function render() {
+    //get the frame
+    requestAnimationFrame( render );
+
+    //animate the cube
+    cube.rotation.x += 0.1;
+    cube.rotation.y += 0.1;
+
+    //render the scene
+    renderer.render( scene, camera );
+}
+
+init();
+addCube();
+render();
