03130e8c1c304a66b38546f8773800b1a4fa437e
AlcinaW <white_kaitou@hotmail.com>
2016-04-10 18:49:40 -0400

    load test

--- public/js/app.js
+++ public/js/app.js
@@ -8,107 +8,83 @@
 //to-do: what to do about more than one piece
 //to-do: re-add sliders
 
-  /**
-   * AudioAnalyser
-   * param- soundPath: path to your sound
-   */
-   
-  function AudioAnalyser(soundPath){
-    
-      this.sound =soundPath;
-      this.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-      this.source = this.audioCtx.createBufferSource();        
-      this.request = new XMLHttpRequest();
-      
-      //from https://github.com/srchea/Sound-Visualizer/blob/master/js/audio.js
-      this.source;
-      this.analyser;
-      
-      //publicly accessible music attributes
-      this.boost = 0;
-      
-  }
-  
-  
-   AudioAnalyser.prototype={
-    constructor:AudioAnalyser,
+/* Hoist some variables. */
+var audio, context;
 
-    /**
-    * http://mdn.github.io/decode-audio-data/
-    */
-    // use XHR to load an audio track, and
-    // decodeAudioData to decode it and stick it in a buffer.
-    // Then we put the buffer into the source
-    loadAudio:function(){
+/* Try instantiating a new AudioContext, throw an error if it fails. */
+try {
+    /* Setup an AudioContext. */
+    context = new AudioContext();
+} catch(e) {
+    throw new Error('The Web Audio API is unavailable');
+}
 
-        this.request.open('GET', this.sound, true);
-        this.request.responseType = 'arraybuffer';
-        
-        var audioAnalyserInstance = this;
-        //on load decode the audio
-        this.request.onload = function() {
-          audioAnalyserInstance.decodeAudio();
+/* Create a script processor node with a `bufferSize` of 1024. */
+var processor = context.createScriptProcessor(1024),
+    /* Create an analyser node */
+    analyser = context.createAnalyser();
+
+/* Wire the processor into our audio context. */
+processor.connect(context.destination);
+/* Wire the analyser into the processor */
+analyser.connect(processor);
+
+/* Define a Uint8Array to receive the analysers data. */
+var data = new Uint8Array(analyser.frequencyBinCount);
+
+/* Define a `Sound` Class */
+var Sound = {
+    /* Give the sound an element property initially undefined. */
+    element: undefined,
+    /* Define a class method of play which instantiates a new Media Element
+     * Source each time the file plays, once the file has completed disconnect 
+     * and destroy the media element source. */
+    play: function() {
+        var sound = context.createMediaElementSource(this.element);
+        this.element.onended = function() {
+            sound.disconnect();
+            sound = null;
+            /* Noop the audioprocess handler when the file finishes. */
+            processor.onaudioprocess = function() {};
         }
-        
-        //then send the request
-        this.request.send();  
-      
-    },
-    decodeAudio:function(){
-      
-        var audioAnalyserInstance = this;
-        this.audioCtx.decodeAudioData(this.request.response, function(decodedData) {
-          
-            audioAnalyserInstance.setUpAnalyser(decodedData);
-        },
-        function(e){"Error with decoding audio data" + e.err});
-    },
-    setUpAnalyser:function(buffer){
-          
-         var audioAnalyserInstance = this;
-          /**from https://github.com/srchea/Sound-Visualizer/blob/master/js/audio.js */
-          var scriptProcessorNode = this.audioCtx.createScriptProcessor(2048, 1, 1);
-          scriptProcessorNode.buffer = buffer;
-          scriptProcessorNode.connect(this.audioCtx.destination);
-          
-          this.analyser = this.audioCtx.createAnalyser();
-          this.analyser.smoothingTimeConstant = 0.6;
-          this.analyser.fftSize = 512;
-          this.analyser.connect(scriptProcessorNodes);
-          
-          this.source.buffer = buffer;
-          this.source.connect(this.audioCtx.destination);
-          this.source.loop = true;
-          this.source.connect(this.analyser);
-          
-          scriptProcessorNode.onaudioprocess = function(e) {
-              audioAnalyserInstance.analyseBoost();
-           }
+        /* Add the following line to wire into the analyser. */
+        sound.connect(analyser);
+        sound.connect(context.destination);
 
-      
-    },
-    analyseBoost:function(){
-      
-          /**from https://github.com/srchea/Sound-Visualizer/blob/master/js/audio.js */      
-          var audioArray = new Uint8Array(this.analyser.frequencyBinCount);
-          
-          this.analyser.getByteFrequencyData(audioArray);
-          
-          this.boost = 0;
-          
-          for (var i = 0; i < audioArray.length; i++) {
-              this.boost += audioArray[i];
-           }
-          
-          this.boost = this.boost / audioArray.length;
-    },
-    
-    /**
-     * play music
-     */
-    play:function(){
-        this.loadAudio();
-        this.source.start(0);
+        processor.onaudioprocess = function() {
+            /* Populate the data array with the frequency data. */
+            data = new Uint8Array(analyser.frequencyBinCount);
+            analyser.getByteTimeDomainData(data);
+        };
+        /* Call `play` on the MediaElement. */
+        this.element.play();
     }
-    
-   }
+};
+
+/* Create an async function which returns a promise of a playable audio element. */
+function loadAudioElement(url) {
+    return new Promise(function(resolve, reject) {
+        var audio = new Audio();
+        audio.addEventListener('canplay', function() {
+            /* Resolve the promise, passing through the element. */
+            resolve(audio);
+        });
+        /* Reject the promise on an error. */
+        audio.addEventListener('error', reject);
+        audio.src = url;
+        audio.crossOrigin = 'anonymous';
+    });
+}
+
+/* Let's load our file. */
+loadAudioElement('../media/Every_Step.mp3').then(function(elem) {
+    /* Instantiate the Sound class into our hoisted variable. */
+    audio = Object.create(Sound);
+    /* Set the element of `audio` to our MediaElement. */
+    audio.element = elem;
+    /* Immediately play the file. */
+    audio.play();
+}, function(elem) {
+    /* Let's throw an the error from the MediaElement if it fails. */
+    throw elem.error;
+});
