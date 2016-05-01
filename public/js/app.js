40dd08f15f0c46a656e0aa66f11b53b798a1d071
AlcinaW <white_kaitou@hotmail.com>
2016-04-30 23:56:53 -0400

    cube test
    no camera control

--- public/js/app.js
+++ public/js/app.js
@@ -267,39 +267,75 @@ var Colors = {
 };
 
 //START VISUALIZATION
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+// var scene = new THREE.Scene();
+// var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+// var renderer = new THREE.WebGLRenderer();
+// renderer.setSize( window.innerWidth, window.innerHeight );
+// document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.IcosahedronGeometry( 2, 0, 1 );
+// var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
 
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// var cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
 
-// create the material 
-//var material = new THREE.MeshPhongMaterial({
-    //color:0x68c3c0,
-    //wireframe: true,
-    //transparent:true,
-    //opacity:.6,
-    //shading:THREE.FlatShading,
-//});
+// camera.position.z = 5;
 
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// var render = function () {
+//     requestAnimationFrame( render );
 
-camera.position.z = 5;
+//     cube.rotation.x += 0.1;
+//     cube.rotation.y += 0.1;
 
-var render = function () {
-    requestAnimationFrame( render );
+//     renderer.render(scene, camera);
+// };
+// render();
 
-    cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
+var scene, camera, renderer;
 
+var WIDTH  = window.innerWidth;
+var HEIGHT = window.innerHeight;
+
+var SPEED = 0.01;
+
+function initialize() {
+    scene = new THREE.Scene();
+
+    initCube();
+    initCamera();
+    initRenderer();
+
+    document.body.appendChild(renderer.domElement);
+}
+
+function initCamera() {
+    camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 1, 10);
+    camera.position.set(0, 3.5, 5);
+    camera.lookAt(scene.position);
+}
+
+function initRenderer() {
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(WIDTH, HEIGHT);
+}
+
+function initCube() {
+    cube = new THREE.Mesh(new THREE.CubeGeometry(2, 2, 2), new THREE.MeshNormalMaterial());
+    scene.add(cube);
+}
+
+function rotateCube() {
+    cube.rotation.x -= SPEED * 2;
+    cube.rotation.y -= SPEED;
+    cube.rotation.z -= SPEED * 3;
+}
+
+function render() {
+    requestAnimationFrame(render);
+    rotateCube();
     renderer.render(scene, camera);
-};
+}
+
+initialize();
 render();
-
-
